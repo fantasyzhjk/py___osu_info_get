5f757b7292718645632ac3dac64bc623b25e77b8
Adsicmes <1483492332@qq.com>
2021-04-03 18:23:22 +0800

    zh救救我吧！*1： 请求次数过多

--- Firstuse.py
+++ Firstuse.py
@@ -1,17 +1,37 @@
-"""第一次使用的基本设置"""
+"""第一次使用的基本设置，查看可以先把能折叠的全部折叠，注释写在外边了"""
 
 import json
+import time
 
+print('''该程序用于设置基本信息，和初始化token.json（不初始化使用main.py的特定功能会百分百报错）
+
+注：1.一旦设置请不要更改，否则数据会乱，如要更改用户名，请另外下载一份。
+   2.该程序可以随意更改文件夹，但是要保证路径不带有中文，且移动整个文件夹
+   
+   按下回车开始设置''')  # 使用说明
+input()
+
+
+# 用户输入basicinfo Done
 user = str(input("请输入玩家名："))
-v2id = str(input("\n请输入你oauth程序的id："))
-v2pw = str(input("\n请输入你oauth程序的密钥："))
+v2id = str(input("请输入你oauth程序的id："))
+v2pw = str(input("请输入你oauth程序的密钥："))
+userid = str(input("请输入玩家的id："))
 
-basicinfo = {"user":user,"v2id":v2id,"v2pw":v2pw}
 
+# 将basicinfo写入文件
+basicinfo = {"user": user, "userid": userid, "v2id": v2id, "v2pw": v2pw}
 basicinfo_json = json.dumps(basicinfo)
+filebasicinfo = open("data/basicinfo.json", mode="w")
+filebasicinfo.write(basicinfo_json)
+filebasicinfo.close()
 
-file = open("data/basicinfo.json",mode="w")
+# 初始化token.json
+datenow = time.strftime("%Y-%m-%d")
+tokenjson = {'token': "0000000000", 'getdate': datenow}
+tokenfile = json.dumps(tokenjson)
+filetoken = open("data/token.json", mode='w')
+filetoken.write(tokenfile)
+filetoken.close()
 
-file.write(basicinfo_json)
-
-input('\n尝试写入完成，请查看data文件夹下的basicinfo.json文件进行检查\n\n按下回车退出')
+input('''\n尝试写入完成，请查看data文件夹下的basicinfo.json和token.json文件进行检查\n\n按下回车退出''')
--- main.py
+++ main.py
@@ -1,16 +1,63 @@
-"""程序主体"""
+"""程序主体,查看可以先把能折叠的全部折叠，注释写在外边了"""
 import json
+import time
+import requests
 
+
+# 获取基本信息 Done
 def getbasicinfo(a):
     data = json.load(open('data/basicinfo.json'))
     if a == 'name':
-        result = data[0]
+        result = data['user']
+    elif a == 'userid':
+        result = data['userid']
     elif a == 'id':
-        result = data[1]
+        result = data['v2id']
     elif a == 'pw':
-        result = data[2]
+        result = data['v2pw']
     else:
         print('''请检查main.py是否有错误
         此处的错误指调用getbasicinfo模块时参数是否为"id"、"name"或"pw"
         不懂请在github上提交issue''')
-    return(result)
+    return result
+
+
+# 检测token.json里的token是否可用，不可用则获取token并重新写入 -ing
+def gettoken(id, pw):
+    tokenfile = json.load(open('data/token.json'))
+    datenow = time.strftime("%Y-%m-%d")
+    if tokenfile['getdate'] != datenow or tokenfile['token'] == '0000000000':
+        # 获取新token
+        url = 'https://osu.ppy.sh/oauth/token'
+        headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+        body = {"grant_type": "client_credentials", "client_id": id, "client_secret": pw, "scope": "public"}
+        requests.DEFAULT_RETRIES = 5  # 增加重试连接次数
+        s = requests.session()
+        s.keep_alive = False  # 关闭多余连接
+        index = s.get(url, headers=headers, data=body, timeout=300)
+        tokenjsontext = index.text
+        jsonn = json.loads(tokenjsontext)
+        token = jsonn['accesstoken']
+        # 写入token.json
+        tokenWhichIsJson = {'token': token, 'getdate': datenow}
+        whichToWrite = json.dumps(tokenWhichIsJson)
+        tokenjson = open('data/token.json', mode='w')
+        tokenjson.write(whichToWrite)
+    else:
+        file = json.load(open('data/token.json'))
+        token = file['token']
+    return token
+
+
+oauthpw = getbasicinfo('pw')
+oauthid = getbasicinfo('id')
+username = getbasicinfo('name')
+userid = getbasicinfo('userid')
+
+token = gettoken(oauthid, oauthpw)
+
+print('username:' + username)
+print('userid' + userid)
+print('oauthid:' + oauthid)
+print('oauthpw:' + oauthpw)
+print('token:' + token)

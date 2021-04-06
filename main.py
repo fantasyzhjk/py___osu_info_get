d755f0bcc80b1696db8b21f986238820f535c9aa
Adsicmes <1483492332@qq.com>
2021-04-06 23:13:06 +0800

    第二个功能上线

--- main.py
+++ main.py
@@ -32,8 +32,10 @@ def get0token(id, pw):
     if tokenfile['getdate'] != datenow or tokenfile['token'] == '0000000000':
         # 获取新token
         url = 'https://osu.ppy.sh/oauth/token'
-        headers = {'Accept': 'application/json', 'Content-Type': 'application/json', 'Connection': 'close'}
-        body = {"grant_type": "client_credentials", "client_id": id, "client_secret": pw, "scope": "public"}
+        headers = {'Accept': 'application/json',
+                   'Content-Type': 'application/json', 'Connection': 'close'}
+        body = {"grant_type": "client_credentials",
+                "client_id": id, "client_secret": pw, "scope": "public"}
         index = requests.post(url, headers=headers, json=body, timeout=300)
         tokenjsontext = index.text
         jsonn = json.loads(tokenjsontext)
@@ -58,7 +60,8 @@ def getbeatmap(bid, accesstoken):
     headers = {"Accept": "application/json", "Content-Type": "application/json",
                'Authorization': 'Bearer ' + accesstoken, 'Connection': 'close'}
     beatmap_url_to_get = 'https://osu.ppy.sh/api/v2/beatmaps/' + bid
-    beatmap_url_get_result = requests.get(url=beatmap_url_to_get, headers=headers)
+    beatmap_url_get_result = requests.get(
+        url=beatmap_url_to_get, headers=headers)
     beatmap_get_result = beatmap_url_get_result.text
     beatmap_json = json.loads(beatmap_get_result)
     out = """-------------------------------------------------------
@@ -113,16 +116,55 @@ def getuser(userid, accesstoken):
     user_json = json.loads(user_get_result)
     # print (user_json) 好家伙zh牛逼
     out = """-------------------------------------------------------
-{username}
-总PP：{statistics[pp]}
-游戏排名：{statistics[global_rank]}
-游戏次数：{statistics[play_count]}
-准确率：{statistics[hit_accuracy]}%
-Rank分数：{statistics[ranked_score]}
-总分数：{statistics[total_score]}
--------------------------------------------------------
+    {username}
+    总PP：{statistics[pp]}
+    游戏排名：{statistics[global_rank]}
+    游戏次数：{statistics[play_count]}
+    准确率：{statistics[hit_accuracy]}%
+    Rank分数：{statistics[ranked_score]}
+    总分数：{statistics[total_score]}
+    -------------------------------------------------------
     """.format(**user_json)
-    return out
+    print(out)
+    more_info_sec = str(input("想要更详细的信息？按下Y再回车吧！(也可以直接回车走人哦)"))
+    if more_info_sec == 'Y' or more_info_sec == 'y':
+        clearaaaaa = os.system("cls")  # 清屏
+        print("""-------------------------------------------------------
+        用户名：{username}
+        用户id：{id}
+        头像：{avatar_url}
+        地区：{location}
+        注册时间：{join_date}
+        游玩模式：{playmode}
+        设备：{playstyle}
+        喜欢的铺面数：{loved_beatmapset_count}
+        曾用名：{previous_usernames}
+        最近是否活跃：{is_active}
+        目前是否在线：{is_online}
+        是否为撒泼特：{is_supporter}
+        兴趣：{interests}
+
+        等级：{statistics[level][current]}
+        PP：{statistics[pp]}
+        Rank分数：{statistics[ranked_score]}
+        总分数：{statistics[total_score]}
+        准确率：{statistics[hit_accuracy]}%
+        游玩数：{statistics[play_count]}
+        游玩时间：{statistics[play_time]}s （以后优化）
+        世界排名：{statistics[global_rank]}
+        国内排名：{statistics[country_rank]}
+        最高连击：{statistics[maximum_combo]}x
+
+        SS数：{statistics[grade_counts][ss]}
+        SSH数：{statistics[grade_counts][ssh]}
+        S数：{statistics[grade_counts][s]}
+        SH数：{statistics[grade_counts][sh]}
+        A数：{statistics[grade_counts][a]}
+        -------------------------------------------------------
+        """.format(**user_json))
+    else:
+        pass
+    return
 
 
 """-------------------------------程序主体-------------------------------"""
@@ -147,15 +189,14 @@ if functionselect == '1':
     output = getbeatmap(bid, token)
     print(output)
 elif functionselect == '2':
-    playerselect = str(input('1.你自己\n2.其他玩家'))
+    playerselect = str(input('1.你自己\n2.其他玩家\n你的选择：'))
     if playerselect == '1':
         playertoget = userid
     elif playerselect == '2':
         playertoget = str(input('请输入玩家id(注意，是id)：'))
     else:
-        input('参数错误，请重新运行')
+        input('参数错误，请重新运行\n按下回车退出')
         exit(0)
-    output = getuser(playertoget, token)
-    print(output)
+    getuser(playertoget, token)
 
 input('回车键退出')
